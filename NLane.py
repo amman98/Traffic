a269d36bbc34f4f7937a26d3d227f41e56106e4d
sherka2 <60676658+sherka2@users.noreply.github.com>
2020-03-05 12:41:59 -0800

    Create NLane.py

---
+++ NLane.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  5 11:40:13 2020
+
+@author: esher
+"""
+
+class NLane(object):
+   
+    def __init__(self,x=7, y=4,lLane,rLane,ftLane):
+        self.x=x
+        self.y=y
+        self.carList=[]
+        #leftLane
+        self.lLane=lLane
+        #rightLane
+        self.rLane=rLane
+        #lane to the front
+        self.sLane=fLane
+    
+    
+        
+        
+    def addCar(self,car):
+        self.carList.append(car)
+    
+    def removeCar(self, car):
+        
+        self.carList.remove(car)
+    
+    def turnRight(self, car):
+        car.x=car.x+1
+        car.y=car.y+2
+        rLane.addCar(car)
+        self.removeCar(car)
+    
+    def turnLeft(self,car):
+        car.x=car.x-2
+        car.y=car.y-2
+        lLane.addCar(car)
+        self.removeCar(car)
+    
+    def goStraight(self,car):
+        car.x=car.x+2
+        
+        
+        
+        
+    def moveCar(self, car):
+        if (light="red"):
+            pass
+        if(car.car_move=="right"):
+            self.turnRight(car)
+        elif( car.move=="left"):
+            self.turnLeft(car)
+        else:
+            self.goStraight(car)
+            
+        #returns false if 
+        bool False
+        
+        
+    #cars that reach boundary 
+        
+        
+        

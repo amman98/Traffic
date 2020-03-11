433333492ebd224de9e6c88d1644a11a3f25bb1a
James Pfleger <44912424+PflegerJ@users.noreply.github.com>
2020-03-11 00:58:28 -0700

    Update StraightLane.py

--- StraightLane.py
+++ StraightLane.py
@@ -49,187 +49,187 @@ class StraightLane(object):
              A pointer to the next lane if the car travels right and then
                 wants to take a left turn next.
          """
-    def __init__(self, direction, probRight, probLeft carLimit = 10):
+    def __init__(self, direction, carLimit = 10):
         
         self.carLimit = carLimit
         self.carCount = 0
         self.direction = direction
         self.carList = []
-        self.probRight = probRight
-        self.probLeft = probLeft
         self.nextLaneFwd = None
         self.nextLaneFwdLeft = None 
         self.nextLaneRightFwd = None           
         self.nextLaneRightLeft = None
        
-    def moveCars(self, greenLight = True):
+    def moveCars(self):
         carLeft = False
         # check that the lane is not empty
         if self.carList.len() != 0:
-            self.carList[0].timeToBorder = self.carList[0].timeToBorder  + 1
-            # check that the light for the lane is green
-            if greenLight:                
-                #- update car's coordinates based on direction lane is facing
-                
-                # car wants to go straight throught the intersection
-                if self.carList[0].car_turn == "STRAIGHT":
-                    
-                    # car wants to go straight or right in the next intersection
-                    self.carList[0].chooseTurn(probRight, probLeft)
-                    if self.carList[0].car_turn == "STRAIGHT" or \
-                        self.carList[0].car_turn == "RIGHT":
-                        if self.nextLaneFwd.carList.len() != \
-                            self.nextLaneFwd.carLimit:
-                            # car is traveling North
-                            if self.direction == 'NORTH':
-                                self.carList[0].loc_in_environ[0] = \
-                                    self.carList[0].loc_in_environ[0] - 1
-                            
-                            # car is traveling East
-                            elif self.direction == 'EAST':
-                                self.carList[0].loc_in_environ[1] = \
-                                    self.carList[0].loc_in_environ[1] + 1
-                            
-                            # car is traveling West
-                            elif self.direction == 'WEST':
-                                self.carList[0].loc_in_environ[1] = \
-                                    self.carList[0].loc_in_environ[1] - 1
-                                
-                            # car is traveling South
-                            elif self.direction == 'SOUTH':
-                                self.carList[0].loc_in_environ[0] = \
-                                    self.carList[0].loc_in_environ[0] + 1
-                
-                            # adding the car to the new lane
-                            self.carList[0].environ = self.nextLaneFwd
-                            self.nextLaneFwd.addCar(self.carList[0])
-                            self.removeCar(self.carList[0])
-                            self.carCount = self.carCount - 1
-                            carLeft = True
-                     
-                    # car wants to turn left at next intersection
-                    # car will enter the left lane immediatly   
-                    else:
-                        # checking for room in the left lane
-                        if self.nextLaneFwdLeft.carList.len() != \
-                            self.nextLandFwdLeft.carLimit:
-                            # car is traveling North
-                            if self.direction == 'NORTH':
-                                self.carList[0].loc_in_environ[0] = \
-                                    self.carList[0].loc_in_environ[0] - 1
-                            
-                            # car is traveling East
-                            elif self.direction == 'EAST':
-                                self.carList[0].loc_in_environ[1] = \
-                                    self.carList[0].loc_in_environ[1] + 1
-                            
-                            # car is traveling West
-                            elif self.direction == 'WEST':
-                                self.carList[0].loc_in_environ[1] = \
-                                    self.carList[0].loc_in_environ[1] - 1
-                                
-                            # car is traveling South
-                            elif self.direction == 'SOUTH':
-                                self.carList[0].loc_in_environ[0] = \
-                                    self.carList[0].loc_in_environ[0] + 1
-                
-                            # adding the car to the new lane
-                            self.carList[0].environ = self.nextLaneFwdLeft
-                            self.nextLaneFwdLeft.addCar(self.carList[0])
-                            self.removeCar(self.carList[0])
-                            self.carCount = self.carCount - 1
-                            carLeft = True
-                        
-                # car is turning right
-                else:
-                    
-                    # need to decide next movement to know which lane to enter
-                    self.carList[0].chooseTurn(probRight, probLeft )
-                    if self.carList[0].car_turn == "STRAIGHT" or \
-                        self.carList[0].car_turn == "RIGHT":
-                        if self.nextLaneRightFwd.carList.len() != \
-                            self.nextLaneRightFwd.carLimit:
-                            # car is traveling North
-                            if self.direction == 'NORTH':
-                                self.carList[0].loc_in_environ[0] = \
-                                    self.carList[0].loc_in_environ[0] - 1
-                                self.carList[0].loc_in_environ[1] = \
-                                    self.carList[0].loc_in_environ[1] + 1
-                            
-                            # car is traveling East
-                            elif self.direction == 'EAST':
-                                self.carList[0].loc_in_environ[0] = \
-                                    self.carList[0].loc_in_environ[0] + 1
-                                self.carList[0].loc_in_environ[1] = \
-                                    self.carList[0].loc_in_environ[1] + 1
-                            
-                            # car is traveling West
-                            elif self.direction == 'WEST':
-                                self.carList[0].loc_in_environ[0] = \
-                                    self.carList[0].loc_in_environ[0] - 1
-                                self.carList[0].loc_in_environ[1] = \
-                                    self.carList[0].loc_in_environ[1] - 1
-                                
-                            # car is traveling South
-                            elif self.direction == 'SOUTH':
-                                self.carList[0].loc_in_environ[0] = \
-                                    self.carList[0].loc_in_environ[0] + 1
-                                self.carList[0].loc_in_environ[1] = \
-                                    self.carList[0].loc_in_environ[1] + 1
-                
-                            # adding the car to the new lane
-                            self.carList[0].environ = self.nextLaneRightFwd
-                            self.nextLaneRightFwd.addCar(self.carList[0])
-                            self.removeCar(self.carList[0])
-                            self.carCount = self.carCount - 1
-                            carLeft = True
-                     
-                    # car wants to turn left at next intersection
-                    # car will enter the left lane immediatly   
-                    else:
-                        # checking for room in the left lane
-                        if self.nextLaneRightLeft.carList.len() != \
-                            self.nextLandRightLeft.carLimit:
-                            # car is traveling North
-                            if self.direction == 'NORTH':
-                                self.carList[0].loc_in_environ[0] = \
-                                    self.carList[0].loc_in_environ[0] - 1
-                                self.carList[0].loc_in_environ[1] = \
-                                    self.carList[0].loc_in_environ[1] + 1
-                            
-                            # car is traveling East
-                            elif self.direction == 'EAST':
-                                self.carList[0].loc_in_environ[0] = \
-                                    self.carList[0].loc_in_environ[0] + 1
-                                self.carList[0].loc_in_environ[1] = \
-                                    self.carList[0].loc_in_environ[1] + 1
-                            
-                            # car is traveling West
-                            elif self.direction == 'WEST':
-                                self.carList[0].loc_in_environ[0] = \
-                                    self.carList[0].loc_in_environ[0] - 1
-                                self.carList[0].loc_in_environ[1] = \
-                                    self.carList[0].loc_in_environ[1] - 1
-                                
-                            # car is traveling South
-                            elif self.direction == 'SOUTH':
-                                self.carList[0].loc_in_environ[0] = \
-                                    self.carList[0].loc_in_environ[0] + 1
-                                self.carList[0].loc_in_environ[1] = \
-                                    self.carList[0].loc_in_environ[1] + 1
-                
-                            # adding the car to the new lane
-                            self.carList[0].environ = self.nextLaneRightLeft
-                            self.nextLaneRightLeft.addCar(self.carList[0])
-                            self.removeCar(self.carList[0])
-                            self.carCount = self.carCount - 1
-                            carLeft = True
-                              
+             #- update car's coordinates based on direction lane is facing
+
+             # car wants to go straight throught the intersection
+             if self.carList[0].car_turn == "STRAIGHT":
+
+                 # car wants to go straight or right in the next intersection
+                 self.carList[0].chooseTurn(probRight, probLeft)
+                 if self.carList[0].car_turn == "STRAIGHT" or \
+                     self.carList[0].car_turn == "RIGHT":
+                     if self.nextLaneFwd.carList.len() != \
+                         self.nextLaneFwd.carLimit:
+                         # car is traveling North
+                         if self.direction == 'NORTH':
+                             self.carList[0].loc_in_environ[0] = \
+                                 self.carList[0].loc_in_environ[0] - 1
+
+                         # car is traveling East
+                         elif self.direction == 'EAST':
+                             self.carList[0].loc_in_environ[1] = \
+                                 self.carList[0].loc_in_environ[1] + 1
+
+                         # car is traveling West
+                         elif self.direction == 'WEST':
+                             self.carList[0].loc_in_environ[1] = \
+                                 self.carList[0].loc_in_environ[1] - 1
+
+                         # car is traveling South
+                         elif self.direction == 'SOUTH':
+                             self.carList[0].loc_in_environ[0] = \
+                                 self.carList[0].loc_in_environ[0] + 1
+
+                         # adding the car to the new lane
+                         self.carList[0].environ = self.nextLaneFwd
+                         self.carList[0].timeToBorder = self.carList[0].timeToBorder  + 1
+
+                         self.nextLaneFwd.addCar(self.carList[0])
+                         self.removeCar(self.carList[0])
+                         self.carCount = self.carCount - 1
+                         carLeft = True
+
+                 # car wants to turn left at next intersection
+                 # car will enter the left lane immediatly   
+                 else:
+                     # checking for room in the left lane
+                     if self.nextLaneFwdLeft.carList.len() != \
+                         self.nextLandFwdLeft.carLimit:
+                         # car is traveling North
+                         if self.direction == 'NORTH':
+                             self.carList[0].loc_in_environ[0] = \
+                                 self.carList[0].loc_in_environ[0] - 1
+
+                         # car is traveling East
+                         elif self.direction == 'EAST':
+                             self.carList[0].loc_in_environ[1] = \
+                                 self.carList[0].loc_in_environ[1] + 1
+
+                         # car is traveling West
+                         elif self.direction == 'WEST':
+                             self.carList[0].loc_in_environ[1] = \
+                                 self.carList[0].loc_in_environ[1] - 1
+
+                         # car is traveling South
+                         elif self.direction == 'SOUTH':
+                             self.carList[0].loc_in_environ[0] = \
+                                 self.carList[0].loc_in_environ[0] + 1
+
+                         # adding the car to the new lane
+                         self.carList[0].environ = self.nextLaneFwdLeft
+                         self.carList[0].timeToBorder = self.carList[0].timeToBorder  + 1
+                         self.nextLaneFwdLeft.addCar(self.carList[0])
+                         self.removeCar(self.carList[0])
+                         self.carCount = self.carCount - 1
+                         carLeft = True
+
+             # car is turning right
+             else:
+
+                 # need to decide next movement to know which lane to enter
+                 self.carList[0].chooseTurn(probRight, probLeft )
+                 if self.carList[0].car_turn == "STRAIGHT" or \
+                     self.carList[0].car_turn == "RIGHT":
+                     if self.nextLaneRightFwd.carList.len() != \
+                         self.nextLaneRightFwd.carLimit:
+                         # car is traveling North
+                         if self.direction == 'NORTH':
+                             self.carList[0].loc_in_environ[0] = \
+                                 self.carList[0].loc_in_environ[0] - 1
+                             self.carList[0].loc_in_environ[1] = \
+                                 self.carList[0].loc_in_environ[1] + 1
+
+                         # car is traveling East
+                         elif self.direction == 'EAST':
+                             self.carList[0].loc_in_environ[0] = \
+                                 self.carList[0].loc_in_environ[0] + 1
+                             self.carList[0].loc_in_environ[1] = \
+                                 self.carList[0].loc_in_environ[1] + 1
+
+                         # car is traveling West
+                         elif self.direction == 'WEST':
+                             self.carList[0].loc_in_environ[0] = \
+                                 self.carList[0].loc_in_environ[0] - 1
+                             self.carList[0].loc_in_environ[1] = \
+                                 self.carList[0].loc_in_environ[1] - 1
+
+                         # car is traveling South
+                         elif self.direction == 'SOUTH':
+                             self.carList[0].loc_in_environ[0] = \
+                                 self.carList[0].loc_in_environ[0] + 1
+                             self.carList[0].loc_in_environ[1] = \
+                                 self.carList[0].loc_in_environ[1] + 1
+
+                         # adding the car to the new lane
+                         self.carList[0].environ = self.nextLaneRightFwd
+                         self.carList[0].timeToBorder = self.carList[0].timeToBorder  + 1
+                         self.nextLaneRightFwd.addCar(self.carList[0])
+                         self.removeCar(self.carList[0])
+                         self.carCount = self.carCount - 1
+                         carLeft = True
+
+                 # car wants to turn left at next intersection
+                 # car will enter the left lane immediatly   
+                 else:
+                     # checking for room in the left lane
+                     if self.nextLaneRightLeft.carList.len() != \
+                         self.nextLandRightLeft.carLimit:
+                         # car is traveling North
+                         if self.direction == 'NORTH':
+                             self.carList[0].loc_in_environ[0] = \
+                                 self.carList[0].loc_in_environ[0] - 1
+                             self.carList[0].loc_in_environ[1] = \
+                                 self.carList[0].loc_in_environ[1] + 1
+
+                         # car is traveling East
+                         elif self.direction == 'EAST':
+                             self.carList[0].loc_in_environ[0] = \
+                                 self.carList[0].loc_in_environ[0] + 1
+                             self.carList[0].loc_in_environ[1] = \
+                                 self.carList[0].loc_in_environ[1] + 1
+
+                         # car is traveling West
+                         elif self.direction == 'WEST':
+                             self.carList[0].loc_in_environ[0] = \
+                                 self.carList[0].loc_in_environ[0] - 1
+                             self.carList[0].loc_in_environ[1] = \
+                                 self.carList[0].loc_in_environ[1] - 1
+
+                         # car is traveling South
+                         elif self.direction == 'SOUTH':
+                             self.carList[0].loc_in_environ[0] = \
+                                 self.carList[0].loc_in_environ[0] + 1
+                             self.carList[0].loc_in_environ[1] = \
+                                 self.carList[0].loc_in_environ[1] + 1
+
+                         # adding the car to the new lane
+                         self.carList[0].environ = self.nextLaneRightLeft
+                         self.carList[0].timeToBorder = self.carList[0].timeToBorder  + 1
+                         self.nextLaneRightLeft.addCar(self.carList[0])
+                         self.removeCar(self.carList[0])
+                         self.carCount = self.carCount - 1
+                         carLeft = True
+
 
         #- call helper method to move rest of traffic forward if a car left
         # the lane
         if carLeft:
-            self.updatePosition(self)
+            self.updatePosition()
                 
     def updatePosition(self):
         """
@@ -237,7 +237,7 @@ class StraightLane(object):
             forward depending on the facing of the lane.
         """
         for inCar in self.carList:
-            inCar. timeAlive = self.carList[0].timeAlive + 1
+            inCar.timeToBorder = inCar.timeToBorder + 1
             if self.direction == 'NORTH':
                 inCar.loc_in_environ[0] = inCar.loc_in_environ[0] - 1
             elif self.direction == 'EAST':
